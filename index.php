8ee45711af118f0d783a41c342d6fcd6bfc28650
asraavisena <asra.avisena@yahoo.com>
2020-09-29 14:36:20 +0200

    delete all ursl when dont have user and at moment with interval 2days

--- index.php
+++ index.php
@@ -20,10 +20,32 @@
 
         </div>
         <hr>
-
+        <?php 
+            $query_url = "SELECT * FROM urls";
+            $select_all_urls_query = mysqli_query($connection, $query_url);
+            while($row = mysqli_fetch_assoc($select_all_urls_query)){
+                $url_long = $row['url_long'];
+                $url_base = $row['url_base'];
+                $url_author = $row['url_author'];
+                $url_date = $row['url_date'];
+            }
+            if(isset($_GET['delete_urls'])) {
+                // echo "<h1>Working</h1>";
+                $the_url_id = $_GET['delete_urls'];
+                $query = "DELETE FROM urls WHERE url_author = 'null' AND url_date < DATE_SUB(NOW(), INTERVAL 2 DAY) ";
+                $delete_query = mysqli_query($connection, $query);
+                header("Location: index.php");
+            }  
+        ?>
         <div class="container">
             <?php include "overview.php";?>
         </div>
+
+        <form action="" method="get" enctype="multipart/form-data">
+            <input type="submit" name="delete_urls" value="Delete" id="btn-login" class="btn btn-custom btn-danger">
+            <span>This button will delete URLs if they dont have user and 30 days old</span>
+            <!-- <a onClick=\"javascript:return confirm('Are you sure want to delete?')\" href='your_urls.php?delete={$url_id}'>Delete</a> -->
+        </form>
         <hr>
 
 <?php include "includes/footer.php"; ?>
